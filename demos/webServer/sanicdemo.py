9f2aae945f1791d3a38d511d2aa5d2b1726e1467
carreymu <muwei1984@163.com>
2020-03-08 11:47:31 +0800

    update sanic

--- demos/webServer/sanicdemo.py
+++ demos/webServer/sanicdemo.py
@@ -1,6 +1,9 @@
 from sanic import Sanic
-from sanic.response import json, text, html
+from sanic.response import json, text, html, stream
+from sanic import response
 # from sanic_cors import CORS
+import asyncio
+import time
 import os
 
 app = Sanic('mySanic')
@@ -64,8 +67,33 @@ async def test(request):
         # "query_args": request.query_args,
     })
 
+@app.route("/files", methods=['POST'])
+async def post_json(request):
+    test_file = request.files.get('file1')
+    file_parameters = {
+        'body': len(test_file.body),
+        'name': test_file.name,
+        'type': test_file.type,
+    }
+
+    return json({
+        "received": True,
+        "file_object_names": request.files.keys(),
+        "test_file_parameters": file_parameters
+    })
+
+@app.route('/stream')
+async def streaming(request):
+    async def streaming_fn(response):
+        await response.write('Welcom to @{}\n'.format(time.strftime('%H:%M:%S')))
+        await asyncio.sleep(3)
+        await response.write('Hello Python @{}\n'.format(time.strftime('%H:%M:%S')))
+    return stream(
+        streaming_fn,
+        headers={'X-Serverd-By': 'Hello Python'}
+    )
+
 if __name__ == '__main__':
     # open brower and input http://127.0.0.1:8001 in url address
     app.run(host="127.0.0.1", port=8001)
-
 # 
