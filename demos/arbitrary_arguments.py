d66283c7911f1fe20b71c8f97d6006bbd787a0ee
carreymu <muwei1984@163.com>
2020-04-04 20:36:19 +0800

    add test **kwargs

--- demos/arbitrary_arguments.py
+++ demos/arbitrary_arguments.py
@@ -6,7 +6,10 @@ def f(*args, con=' & '):
     for arg in args:
         print(arg)
 
-def fc(*args, **kwargs):
+def fc(a2='dd', dc: dict={}, *args, **kwargs):
+    print(f"a2=={a2}")
+    print(f"dc=={dc}")
+    print('=='*10)
     print("args", args)
     print("kargs", kwargs)
     print("fp:{} & Scripts: {},".format(kwargs.get('fp'), "/".join(args)))
@@ -17,6 +20,14 @@ def fc(*args, **kwargs):
     for key, value in kwargs.items():
         print("%s == %s" % (key, value))
 
+def ff(sql_info: dict, fmt: str = "json", **sql_params):
+    print(f"sql_info=={sql_info}")
+    print('==' * 10)
+    print(f"fmt=={fmt}")
+    print('==' * 10)
+    print(sql_params)
+    print('=='*10)
+
 def myf(arg1, arg2, arg3):
     print('arg1:', arg1)
     print('arg2:', arg2)
@@ -26,10 +37,13 @@ if __name__ == "__main__":
     print("============init date *** ============")
     f("python", "c", "C++", con='/')
     print('---')
-    fc("Python", "Javascript", ms="C++", fp="Haskell")
+    fc("Python", {"d1": 1, "d2": "2"}, "Javascript", ms="C++", fp="Haskell")
 
     args = ('python', 'c++', 'java')
     myf(*args)
+
+    fargs={'arg1': 'golang'}
+    ff(fargs, 'df', ms="C++")
 
     kwargs = {'arg1': 'golang', 'arg2': 'c#', 'arg3': 'scala'}
     myf(**kwargs)
