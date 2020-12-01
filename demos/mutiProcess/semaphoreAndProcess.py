5d0a6fe51a055f30099d2068fd82e5665f7413b7
muwei1984@163.com <muwei1984@163.com>
2020-12-01 22:29:11 +0800

    add multiprocess demo

---
+++ demos/mutiProcess/semaphoreAndProcess.py
@@ -0,0 +1,22 @@
+import time
+import os
+from multiprocessing import Process, Semaphore
+
+# Maxmum 4 processes for semaphore
+semaphore = Semaphore(4)
+
+def task(name):
+    if semaphore.acquire():
+        print('pid:%s, name:%s, sleep:1 ..' % (os.getpid(), name))
+        time.sleep(1)
+        semaphore.release()
+
+if __name__ == "__main__":
+    ps = []
+    for i in range(10):
+        p = Process(target=task, args=('p %s' % i, ))
+        ps.append(p)
+        p.start()
+    for p in ps:
+        p.join()
+    
